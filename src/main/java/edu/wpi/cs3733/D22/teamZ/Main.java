d49e1e46e10d9b12b13693df0735139d0347865b
Claire Higginson <clurr555@gmail.com>
2022-04-03 16:48:52 -0400

    getAllMedicalEquipmentByLocation function Claire & Maya

--- src/main/java/edu/wpi/cs3733/D22/teamZ/Main.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/Main.java
@@ -9,6 +9,16 @@ public class Main {
     init.createTables();
     init.populateLocationTable();
     init.populateMedicalEquipmentTable();
+
+//    LocationDAOImpl locDAO = new LocationDAOImpl();
+//    Location loc = locDAO.getLocationByID("zPATI00103");
+//    MedicalEquipmentDAOImpl medDAO = new MedicalEquipmentDAOImpl();
+//    List<MedicalEquipment> medeq = medDAO.getAllMedicalEquipmentByLocation(loc);
+//    for (int i = 0; i < medeq.size(); i++) {
+//      MedicalEquipment temp = medeq.get(i);
+//      System.out.println(temp.getItemID());
+//    }
+
     // init.populateReqTable();
     App.launch(App.class, args);
   }
