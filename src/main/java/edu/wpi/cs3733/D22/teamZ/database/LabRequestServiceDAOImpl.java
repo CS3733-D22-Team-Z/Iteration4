49676f7a90701395d67a1a4621f2e4c669764356
nadiaz2 <nadiaz2@wpi.edu>
2022-04-09 22:01:04 -0400

    Implement import and export csv into LabRequestServiceDAOImpl.java

--- src/main/java/edu/wpi/cs3733/D22/teamZ/database/LabRequestServiceDAOImpl.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/database/LabRequestServiceDAOImpl.java
@@ -1,8 +1,10 @@
 package edu.wpi.cs3733.D22.teamZ.database;
 
 import edu.wpi.cs3733.D22.teamZ.entity.LabServiceRequest;
+import edu.wpi.cs3733.D22.teamZ.entity.MedicalEquipmentDeliveryRequest;
 import edu.wpi.cs3733.D22.teamZ.entity.ServiceRequest;
 import java.io.File;
+import java.io.IOException;
 import java.sql.*;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,6 +16,7 @@ public class LabRequestServiceDAOImpl implements ILabRequestServiceDAO {
 
   static Connection connection = DatabaseConnection.getConnection();
   List<LabServiceRequest> returnList = new ArrayList<>();
+  private LabRequestControlCSV reqCSV;
 
   /**
    * Gets all lab service requests
@@ -29,26 +32,26 @@ public class LabRequestServiceDAOImpl implements ILabRequestServiceDAO {
     try {
       PreparedStatement pstmt =
           connection.prepareStatement(
-              "Select LABRESULT.ITEMID, SERVICEREQUEST.STATUS, ISSUERID, HANDLERID, TARGETLOCATIONID, LABRESULT.TYPE\n"
-                  + "FROM SERVICEREQUEST,LABRESULT where  SERVICEREQUEST.REQUESTID = LABRESULT.ITEMID");
+              "Select LABREQUEST.REQUESTID, SERVICEREQUEST.STATUS, ISSUERID, HANDLERID, TARGETLOCATIONID, LABRESULT.TYPE\n"
+                  + "FROM SERVICEREQUEST,LABRESULT where  SERVICEREQUEST.REQUESTID = LABREQUEST.REQUESTID");
       ResultSet rset = pstmt.executeQuery();
 
       while (rset.next()) {
-        String itemID = rset.getString("ITEMID");
+        String requestID = rset.getString("REQUEST");
         String status = rset.getString("STATUS");
         String issuer = rset.getString("ISSUERID");
         String handler = rset.getString("HANDLERID");
         String targetLoc = rset.getString("TARGETLOCATIONID");
-        String type = rset.getString("TYPE");
+        String labType = rset.getString("LABTYPE");
 
         temp =
             new LabServiceRequest(
-                itemID,
+                requestID,
                 ServiceRequest.RequestStatus.getRequestStatusByString(status),
                 issuer,
                 handler,
                 targetLoc,
-                type);
+                labType);
         // Avoid duplicates
         if (!returnList.contains(temp)) {
           returnList.add(temp);
@@ -73,21 +76,21 @@ public class LabRequestServiceDAOImpl implements ILabRequestServiceDAO {
 
     try {
       PreparedStatement pstmt =
-          connection.prepareStatement("SELECT * FROM LABRESULT WHERE 'ITEMID' = ?");
+          connection.prepareStatement("SELECT * FROM LABREQUEST WHERE 'REQUESTID' = ?");
       pstmt.setString(1, requestID);
       ResultSet rset = pstmt.executeQuery();
 
       rset.next();
       // get the result set
-      if (rset.getString("itemId") == null) {
+      if (rset.getString("requestId") == null) {
         return null;
       }
-      String itemId = rset.getString("itemId");
-      String labType = rset.getString("type");
-      ServiceRequest request = requestDAO.getServiceRequestByID(itemId);
+      String requestId = rset.getString("requestId");
+      String labType = rset.getString("labType");
+      ServiceRequest request = requestDAO.getServiceRequestByID(requestId);
 
       return new LabServiceRequest(
-          itemId,
+          requestId,
           request.getStatus(),
           request.getIssuer(),
           request.getHandler(),
@@ -115,11 +118,9 @@ public class LabRequestServiceDAOImpl implements ILabRequestServiceDAO {
     try {
       PreparedStatement stmt =
           connection.prepareStatement(
-              "INSERT INTO LABRESULT (ITEMID, TYPE, STATUS, CURRENTLOCATION) values (?, ?, ?, ?)");
+              "INSERT INTO LABREQUEST (REQUESTID, LABTYPE) values (?, ?)");
       stmt.setString(1, request.getRequestID());
       stmt.setString(2, request.getLabType());
-      stmt.setString(3, request.getStatus().toString());
-      stmt.setString(4, request.getTargetLocation().getNodeID());
 
       stmt.executeUpdate();
       connection.commit();
@@ -138,6 +139,7 @@ public class LabRequestServiceDAOImpl implements ILabRequestServiceDAO {
    */
   @Override
   public boolean updateLabRequest(LabServiceRequest req) {
+    //TODO implement updateLabRequest
     return false;
   }
 
@@ -150,7 +152,7 @@ public class LabRequestServiceDAOImpl implements ILabRequestServiceDAO {
   @Override
   public boolean deleteLabRequest(LabServiceRequest req) {
     try {
-      PreparedStatement stmt = connection.prepareStatement("DELETE FROM LABRESULT WHERE ITEMID=?");
+      PreparedStatement stmt = connection.prepareStatement("DELETE FROM LABREQUEST WHERE REQUESTID=?");
       stmt.setString(1, req.getRequestID());
       stmt.executeUpdate();
     } catch (SQLException e) {
@@ -164,12 +166,15 @@ public class LabRequestServiceDAOImpl implements ILabRequestServiceDAO {
   /**
    * Exports all LabServiceRequests in the database to specified file location of csv
    *
-   * @param data file location of csv
+   * @param reqData file location of csv
    * @return True if successful, false otherwise
    */
   @Override
-  public boolean exportToLabRequestCSV(File data) {
-    return false;
+  public boolean exportToLabRequestCSV(File reqData) {
+    reqCSV = new LabRequestControlCSV(reqData);
+
+    reqCSV.writeLabRequestCSV(returnList);
+    return true;
   }
 
   /**
@@ -180,6 +185,36 @@ public class LabRequestServiceDAOImpl implements ILabRequestServiceDAO {
    */
   @Override
   public int importLabRequestFromCSV(File data) {
-    return 0;
+    reqCSV = new LabRequestControlCSV(data);
+    int conflictCounter = 0;
+    String temp = "";
+    try {
+      List<LabServiceRequest> tempLabRequest = reqCSV.readLabRequestCSV();
+
+      try {
+        for (LabServiceRequest info : tempLabRequest) {
+          PreparedStatement pstmt =
+                  connection.prepareStatement(
+                          "INSERT INTO LABREQUEST (REQUESTID, LABTYPE) " + "values (?, ?)");
+          temp = info.getRequestID();
+          pstmt.setString(1, info.getRequestID());
+          pstmt.setString(2, info.getLabType());
+
+          // insert it
+          pstmt.executeUpdate();
+        }
+      } catch (SQLException e) {
+        conflictCounter++;
+        System.out.println(
+                "Found "
+                + conflictCounter
+                + " conflicts. "
+                + temp
+                + " is already in the table or does not exist.");
+      }
+    } catch (IOException e) {
+      System.out.println("Failed to populate Lab Equipment Request table");
+    }
+    return conflictCounter;
   }
 }
