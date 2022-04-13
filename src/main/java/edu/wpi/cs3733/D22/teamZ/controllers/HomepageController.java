dc1d298273dc670ed681a75c95382b310d0cdd13
Jake Leavitt <jake.leavitt74@gmail.com>
2022-04-13 00:13:14 -0400

    Fixed welcomeMessage

--- src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/HomepageController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/HomepageController.java
@@ -39,7 +39,7 @@ public class HomepageController implements IMenuAccess {
   public void initialize() {
     String name = MenuController.getLoggedInUser().getName();
     name =
-        name.substring(0, 1).toUpperCase(Locale.ROOT) + name.substring(1).toLowerCase(Locale.ROOT);
+        name.substring(0, 1).toUpperCase(Locale.ROOT) + name.substring(1);
     setWelcomeMessage(name);
   }
 
