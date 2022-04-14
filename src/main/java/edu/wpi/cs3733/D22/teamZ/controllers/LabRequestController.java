9d8da89c808f248ad7a5dfdfe5b84afe280b0eea
Alex Brattstrom <abrattstrom@wpi.edu>
2022-04-14 19:59:49 -0400

    Submit Button Enable/Disable

--- src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/LabRequestController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/LabRequestController.java
@@ -51,6 +51,7 @@ public class LabRequestController extends ServiceRequestController {
         && !patientIdField.getText().trim().isEmpty()
         && !labTypeChoiceBox.getSelectionModel().isEmpty()) {
       submitButton.setDisable(false);
+      System.out.println("Lab Request Submit Button enabled");
     } else {
       submitButton.setDisable(true);
       System.out.println("Lab Request Submit Button disabled");
