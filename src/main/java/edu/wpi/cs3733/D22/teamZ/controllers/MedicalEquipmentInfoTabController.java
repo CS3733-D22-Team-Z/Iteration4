4d8866a8a04994e3f9bb3d1c107dbea521a7be06
Patrick Hunter <pehunter@wpi.edu>
2022-04-16 21:17:36 -0400

    Added CSS to map, including property windows.

--- src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/MedicalEquipmentInfoTabController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/MedicalEquipmentInfoTabController.java
@@ -6,11 +6,11 @@ import edu.wpi.cs3733.D22.teamZ.entity.MapLabel;
 import edu.wpi.cs3733.D22.teamZ.entity.MedicalEquipment;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import io.github.palexdev.materialfx.controls.legacy.MFXLegacyComboBox;
 import java.util.ArrayList;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 
 public class MedicalEquipmentInfoTabController {
@@ -19,7 +19,7 @@ public class MedicalEquipmentInfoTabController {
   @FXML private MFXTextField equipmentTypeField;
   @FXML private MFXTextField equipmentStatusField;
   @FXML private MFXTextField equipmentLocationField;
-  @FXML private MFXLegacyComboBox<String> equipmentComboBox;
+  @FXML private ChoiceBox<String> equipmentComboBox;
   @FXML private Label errorLabel;
   @FXML private MFXTextField equipmentIDField;
   @FXML private MFXButton exitButton;
