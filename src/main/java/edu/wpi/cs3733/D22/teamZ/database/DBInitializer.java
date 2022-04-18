d7f3297b95d65805db3e97c57880dd752c21f41d
Patrick Hunter <pehunter@wpi.edu>
2022-04-17 22:41:23 -0400

    Added finalse

--- src/main/java/edu/wpi/cs3733/D22/teamZ/database/DBInitializer.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/database/DBInitializer.java
@@ -7,14 +7,14 @@ import java.sql.*;
 import java.util.List;
 
 public class DBInitializer {
-  private LocationControlCSV locCSV;
-  private EmployeeControlCSV employeeCSV;
-  private PatientControlCSV patientCSV;
-  private MedicalEquipmentControlCSV medicalEquipmentControlCSV;
-  private ServiceRequestControlCSV serviceControlCSV;
-  private MedEqReqControlCSV medEqReqControlCSV;
-  private MealServReqControlCSV mealServReqControlCSV;
-  private FacadeDAO dao = FacadeDAO.getInstance();
+  private final LocationControlCSV locCSV;
+  private final EmployeeControlCSV employeeCSV;
+  private final PatientControlCSV patientCSV;
+  private final MedicalEquipmentControlCSV medicalEquipmentControlCSV;
+  private final ServiceRequestControlCSV serviceControlCSV;
+  private final MedEqReqControlCSV medEqReqControlCSV;
+  private final MealServReqControlCSV mealServReqControlCSV;
+  private final FacadeDAO dao = FacadeDAO.getInstance();
 
   static Connection connection = EnumDatabaseConnection.CONNECTION.getConnection();
   // DatabaseConnection.getConnection();
