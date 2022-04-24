e515204497aa85879aeaffdafa4fd0d7e9ac20e3
nadiaz2 <nadiaz2@wpi.edu>
2022-04-24 14:23:07 -0400

    Create methods for using TeamBAPI

--- src/main/java/edu/wpi/cs3733/D22/teamZ/apiFacades/SanitationFacadeAPI.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/apiFacades/SanitationFacadeAPI.java
@@ -3,10 +3,31 @@ package edu.wpi.cs3733.D22.teamZ.apiFacades;
 import edu.wpi.cs3733.D22.teamB.api.API;
 import edu.wpi.cs3733.D22.teamB.api.DatabaseController;
 import edu.wpi.cs3733.D22.teamB.api.ServiceException;
+import edu.wpi.cs3733.D22.teamZ.database.FacadeDAO;
+import edu.wpi.cs3733.D22.teamZ.entity.Location;
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SanitationFacadeAPI {
+
+  // Wraps teamB's Location class in a unique class name
+  private static class TeamBLocation extends edu.wpi.cs3733.D22.teamB.api.Location {
+    public TeamBLocation(
+        String nodeID,
+        int xcoord,
+        int ycoord,
+        String floor,
+        String building,
+        String nodeType,
+        String longName,
+        String shortName) {
+      super(nodeID, xcoord, ycoord, floor, building, nodeType, longName, shortName);
+    }
+  }
+
   private static final SanitationFacadeAPI instance = new SanitationFacadeAPI();
+  private final FacadeDAO facadeDAO = FacadeDAO.getInstance();
   private final API api;
   private final DatabaseController apiDatabase;
 
@@ -17,6 +38,20 @@ public class SanitationFacadeAPI {
   private SanitationFacadeAPI() {
     api = new API();
     apiDatabase = new DatabaseController();
+    try {
+      apiDatabase.reset();
+    } catch (ServiceException e) {
+      System.out.println("Failed to clear database");
+    }
+
+    List<Location> locations = facadeDAO.getAllLocations();
+    List<TeamBLocation> bLocations =
+        locations.stream()
+            .map(SanitationFacadeAPI::convertFromLocation)
+            .collect(Collectors.toList());
+    for (TeamBLocation bLoc : bLocations) {
+      apiDatabase.add(bLoc);
+    }
   }
 
   public void run(String cssPath) {
@@ -29,5 +64,22 @@ public class SanitationFacadeAPI {
     } catch (IOException ex) {
       System.out.println("weird");
     }
+  }
+
+
+  private static Location convertToLocation(TeamBLocation bLoc) {
+    return FacadeDAO.getInstance().getLocationByID(bLoc.getNodeID());
+  }
+
+  private static TeamBLocation convertFromLocation(Location loc) {
+    return new TeamBLocation(
+        loc.getNodeID(),
+        loc.getXcoord(),
+        loc.getYcoord(),
+        loc.getFloor(),
+        loc.getBuilding(),
+        loc.getNodeType(),
+        loc.getLongName(),
+        loc.getShortName());
   }
 }
