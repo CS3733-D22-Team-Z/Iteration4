b9668b6cb0d2355a07708597f372034eb1157228
oliviaraisbeck <oeraisbeck@wpi.edu>
2022-04-26 01:02:08 -0400

    graphhhhh

--- src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/FloorDetailsController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/FloorDetailsController.java
@@ -22,7 +22,6 @@ import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
