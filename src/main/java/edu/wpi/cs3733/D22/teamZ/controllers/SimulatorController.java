272b5e902714f23025aa4af029bdab9327e97b7c
oliviaraisbeck <oeraisbeck@wpi.edu>
2022-04-27 00:24:33 -0400

    simulated times work and pause and play buttons, no simuation still lol

--- src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/SimulatorController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamZ/controllers/SimulatorController.java
@@ -10,6 +10,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -19,6 +21,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class SimulatorController implements IMenuAccess, Initializable {
   private MenuController menu;
@@ -30,8 +33,15 @@ public class SimulatorController implements IMenuAccess, Initializable {
   @FXML private MFXButton endSim;
   @FXML private Label clock;
   private MapController mapController;
+  private Timeline timeline;
 
   private Image iconImage;
+
+  DateTimeFormatter timeFormatA = DateTimeFormatter.ofPattern("hh:mm a");
+  long speed;
+  int timesLoop;
+  LocalDateTime simStart;
+  LocalDateTime simEnd;
 
   @Override
   public void setMenuController(MenuController menu) {
@@ -96,8 +106,54 @@ public class SimulatorController implements IMenuAccess, Initializable {
       startSim.setDisable(true);
       pauseSim.setDisable(false);
       endSim.setDisable(false);
-      DateTimeFormatter timeFormatA = DateTimeFormatter.ofPattern("hh:mm a");
-      clock.setText(timeFormatA.format(LocalDateTime.now()));
+
+      if (speedBox.getSelectionModel().getSelectedItem().equals("Real time")) {
+        speed = 1;
+        timesLoop = 86400;
+      } else if (speedBox.getSelectionModel().getSelectedItem().equals("5 min/sec")) {
+        speed = 300;
+        timesLoop = 288;
+      } else if (speedBox.getSelectionModel().getSelectedItem().equals("10 min/sec")) {
+        speed = 600;
+        timesLoop = 144;
+      } else if (speedBox.getSelectionModel().getSelectedItem().equals("30 min/sec")) {
+        speed = 1800;
+        timesLoop = 48;
+      } else if (speedBox.getSelectionModel().getSelectedItem().equals("1 hour/sec")) {
+        speed = 3600;
+        timesLoop = 24;
+      }
+
+      simStart = LocalDateTime.now();
+      simEnd = simStart.plusHours(20);
+
+      clock.setText(timeFormatA.format(simStart));
+      this.timeline = new Timeline(new KeyFrame(Duration.millis(1000), this::Simulate));
+      this.timeline.setCycleCount(timesLoop);
+      this.timeline.play();
     }
+  }
+
+  private void Simulate(ActionEvent actionevent) {
+    simStart = simStart.plusSeconds(speed);
+    clock.setText(timeFormatA.format(simStart));
+  }
+
+  public void pauseSim(ActionEvent actionEvent) {
+    if (pauseSim.getText().equals("Pause Simulation")) {
+      this.timeline.pause();
+      pauseSim.setText("Play Simulation");
+    } else {
+      this.timeline.play();
+      pauseSim.setText("Pause Simulation");
+    }
+  }
+
+  public void endSim(ActionEvent actionEvent) {
+    this.timeline.stop();
+    pauseSim.setDisable(true);
+    endSim.setDisable(true);
+    startSim.setDisable(false);
+    clock.setText("00:00 AM");
   }
 }
